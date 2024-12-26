6fb8d5e8d7652a31eb2c1b705a8fb01f14816667
Kennedy <kennedyotis.nyaga@ingka.ikea.com>
2024-12-26 20:21:52 +0100

    Add middleware for common headers, request logging, and panic recovery; update routes to use new middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("../../ui/static/"))
 
@@ -14,5 +14,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/create", app.newSnippetForm)
 	mux.HandleFunc("POST /snippet/create", app.createSnippet)
 
-	return mux
+	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
 }
